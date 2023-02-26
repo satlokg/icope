5a3985b1db29a6135f00cb790fd037426288db99
Satlok Gupta <satlok.gupta@yahoo.com>
2023-02-26 18:29:43 +0530

    sw

--- resources/views/emails/otp.blade.php
+++ resources/views/emails/otp.blade.php
@@ -4,16 +4,7 @@
     <title>Otp</title>
 </head>
 <body>
-Here is your OTP: 933563
 
-To authenticate, please use the following code.
-
-Do not share your credentials or OTP with anyone on call, email or SMS.
-
-Remember, we're always an email or call away when you need help. Till next time, ciao!
-
-Regards,
-Team ICOPE
     <h1>Here is your OTP: {{ $details['otp'] }}</h1>
     <p>To authenticate, please use the following code.</p>
     <p>Do not share your credentials or OTP with anyone on call, email or SMS.</p>
