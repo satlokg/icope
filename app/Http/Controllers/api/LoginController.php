3c18a0492e9ba9f17b37e2684b79d2ef57594bf6
Satlok Gupta <satlok.gupta@yahoo.com>
2023-03-02 22:51:00 +0530

    sw

--- app/Http/Controllers/api/LoginController.php
+++ app/Http/Controllers/api/LoginController.php
@@ -69,7 +69,7 @@ public function validateOtp(Request $req){
     $p = User::select('id','email','is_pretest_completed','created_at')->where('email', $req->email)->where('email_otp', $req->otp)->first();
     if(strtotime($p->created_at) < strtotime(now())) 
         {
-            return response()->json(['success' => 0, 'message' => 'Otp Expired '], 200);
+            return response()->json(['success' => 'failed', 'message' => 'Otp Expired '], 200);
         }
     if ($p) {
 
