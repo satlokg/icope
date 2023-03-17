48a41f8585c1f27207f2aa6ff2aacc7e194ee664
Satlok Gupta <satlok.gupta@yahoo.com>
2023-03-17 19:06:17 +0530

    sw

--- app/Http/Middleware/Apiauth.php
+++ app/Http/Middleware/Apiauth.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\UserToken;
 use Auth;
 class Apiauth
 {
@@ -20,11 +21,12 @@ class Apiauth
         $token = $request->header('x-auth-key');
         //dd($request->header());
         $did = $request->device_id;
-        $user = User::where('api_token', str_replace('Bearer ', '', $token))->whereHas('token', function($q) use($did){
-            $q->where('device_id',  $did)->whereDate('expire_at', '>', now());
-        })->first();
-        if ($user) {
-            Auth::login($user);
+        // $user = User::where('api_token', str_replace('Bearer ', '', $token))->whereHas('token', function($q) use($did){
+        //     $q->where('device_id',  $did)->whereDate('expire_at', '>', now());
+        // })->first();
+        $userT = UserToken::where('api_token', str_replace('Bearer ', '', $token))->whereDate('expire_at', '>', now())->with('user')->first();
+        if ($userT) {
+            Auth::login($userT->user);
         }
         if (Auth::check() == false) {
             return response()->json(['status' => 'failed', 'message' => 'Unauthenticate'], 401);
